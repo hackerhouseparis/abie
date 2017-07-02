91b232ed81037cb383f5f43c2026f93a55faf165
Julien Béranger <jb@evr.gr>
2017-07-02 14:23:11 +0200

    test vote
    I don't know if the last test (line 37) is valid

--- test/abie.js
+++ test/abie.js
@@ -18,30 +18,31 @@ contract('AbieFund', (accounts)=> {
     .then(result => assert.equal(result, m1))
   })
 
-  it("candidate ask for membership", ()=> {
+  it("candidate ask for membership", () => {
     return abie.askMembership({value: web3.toWei(1, "ether") ,from: candidate})
     .then(() => abie.proposals.call(0))
     .then(result => assert.equal(result[3], candidate))
   })
 
-  it("proposal is published", function() {
+  it("proposal is published", () => {
     return abie.addProposal(0x0, 1, 0x0, {value: web3.toWei(1, "ether") ,from: candidate})
     .then(result => abie.proposals.call(0))
     .then(result => {
-      //result[3] => proposal.recipient
       assert.equal(result[3], candidate, "error add proposal")
       return abie.nbProposalsFund()})
     .then(result => assert.equal(result, 1, "error count proposals"))
   })
 
-  /*it("test vote", function() {
-    return abie.vote(0,1 ,{from: m3})
-    .then(function() {
-      return abie.countAllVotes(0)})
-      .then(function(result) {
-      console.log(result);
-      // result[3] => proposal.recipient
-      assert.equal(result, 1 )
-    })
-  })*/
+  it("test vote", () => {
+      return abie.isValidMember(m1)
+      check = abie.isValidMember(m1)
+      assert.isTrue(check, "m1 is not a valid member")
+      return abie.vote(1, {from: m1})
+      .then(() => {
+        return abie.countAllVotes(0)
+        .then((result) => {
+          assert.equal(result, 1, "error during the vote")
+        })
+     })
+  })
 })
