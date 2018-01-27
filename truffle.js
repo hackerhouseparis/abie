ec6b2edb5f7af8472b9b7690145ea3478ebaade8
Nicolas Wagner <contact@wagner-nicolas.com>
2018-01-28 00:58:10 +0100

    fix build

--- truffle.js
+++ truffle.js
@@ -1,9 +1,16 @@
 module.exports = {
   networks: {
     development: {
+      gas: 2900000,
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+    },
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      } 
     }
   }
 };
