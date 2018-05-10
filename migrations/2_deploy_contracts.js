14c1bfab93106be974507335c556af1ee2b6add5
Julien Béranger <jb@evr.gr>
2018-05-10 12:40:49 +0200

    uncomment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,11 +10,11 @@ Replace the 2 addr manually at line 16.
 ***/
 
 var Abie = artifacts.require("./Abie.sol");
-//var Web3 = require('../node_modules/web3');
+var Web3 = require('../node_modules/web3');
 
 module.exports = function(deployer) {
-	deployer.deploy(Abie,["addr","addr"]);
-		//web3.eth.getAccounts((error, accounts) => {
-		//deployer.deploy(Abie,[accounts[0],accounts[1]])
-	//});
+	//deployer.deploy(Abie,["addr","addr"]);
+		web3.eth.getAccounts((error, accounts) => {
+		deployer.deploy(Abie,[accounts[0],accounts[1]])
+	});
 };
