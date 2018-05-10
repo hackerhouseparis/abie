76b844b45a52aa351b73ebc46abac090bf4905d6
Julien Béranger <jb@evr.gr>
2018-05-10 14:12:56 +0200

    up

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,7 @@
-/***
-
-When using truffle:
-Uncomment lines 13, 17, 18 and 19 and comment line 16 .
-
-When using the interface:
-Uncomment line 16 and comment lines 13, 17, 18 and 19.
-Replace the 2 addr manually at line 16.
-
-***/
-
-var Abie = artifacts.require("./Abie.sol");
-var Web3 = require('../node_modules/web3');
+var Web3 = require('../node_modules/web3')
+const Abie = artifacts.require("Abie")
 
 module.exports = function(deployer) {
-	//deployer.deploy(Abie,["addr","addr"]);
 		web3.eth.getAccounts((error, accounts) => {
 		deployer.deploy(Abie,[accounts[0],accounts[1]])
 	});
